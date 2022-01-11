737a1cc71fa91c06835a1182b74cf22361faf50c
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-11 15:50:00 -0500

    Initial Commit

---
+++ prod.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
