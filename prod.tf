d35149e715923f9c467672a5aeb0010ff0015496
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-18 11:38:22 -0500

    Added three webapps and refactored

--- prod.tf
+++ prod.tf
@@ -7,68 +7,19 @@ terraform {
   }
 }
 
+# BEGIN AND SETUP
+
 provider "azurerm" {
   features {}
 }
 
-#
-## Create a resource group
-#resource "azurerm_resource_group" "HackingExerciseOne" {
-#  name     = "HackingExerciseOne"
-#  location = "East US"
-#
-#  tags = {
-#    "Terraform" : "true"
-#  }
-#}
-#
-#
-#resource "azurerm_app_service_plan" "HackingExerciseOne" {
-#  name                = "HackingExerciseOne-appserviceplan"
-#  location            = azurerm_resource_group.HackingExerciseOne.location
-#  resource_group_name = azurerm_resource_group.HackingExerciseOne.name
-#
-#  sku {
-#    tier = "Free"
-#    size = "F1"
-#  }
-#
-#  tags = {
-#    "Terraform" : "true"
-#  }
-#}
-#
-#resource "azurerm_app_service" "HackingExerciseOne" {
-#  name                = "HackingExerciseOne-app-service"
-#  location            = azurerm_resource_group.HackingExerciseOne.location
-#  resource_group_name = azurerm_resource_group.HackingExerciseOne.name
-#  app_service_plan_id = azurerm_app_service_plan.HackingExerciseOne.id
-#
-#  site_config {
-#    dotnet_framework_version = "v4.0"
-#    #scm_type                 = "VSTSRM"
-#  }
-#
-#  source_control {
-#    repo_url           = "https://github.com/thomaskennedy1066/HackingExampleOne.git"
-#    branch             = "main"
-#    manual_integration = true
-#  }
-#
-#
-#
-#  tags = {
-#    "Terraform" : "true"
-#  }
-#}
-
 variable "namePrefix" {
-  type = string
+  type = list(string)
 }
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.namePrefix}"
+  name     = "rg-${var.namePrefix[0]}"
   location = "eastus"
 
   tags = {
@@ -76,9 +27,10 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+# MAINLINE
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "appserviceplan-${var.namePrefix}"
+  name                = "appserviceplan-${var.namePrefix[0]}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   
@@ -92,19 +44,34 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 
-# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "HackingExerciseOne" {
-  name                = "webapp-${var.namePrefix}"
+# Create WebApps for HackingExercises
+resource "azurerm_app_service" "HackingExercisesOne" {
+  name                = "webapp-${var.namePrefix[0]}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-  
-  source_control {
-    repo_url           = "https://github.com/thomaskennedy1066/nodejs-docs-hello-world.git"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
+
+  tags = {
+    "Terraform" : "true"
   }
+}
+
+resource "azurerm_app_service" "HackingExercisesTwo" {
+  name                = "webapp-${var.namePrefix[1]}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "azurerm_app_service" "HackingExercisesThree" {
+  name                = "webapp-${var.namePrefix[2]}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   tags = {
     "Terraform" : "true"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,9 @@
-namePrefix = "NDCLHackingExercises"
+#namePrefix = "NDCLHackingExercises"
 #namePrefixExOne   = "NDCLHackingExercises"
 #namePrefixExTwo   = "NDCLHackingExercisesTwo"
 #namePrefixExThree = "NDCLHackingExercises-Three"
-
-#namePrefix = ["NDCLHackingExercises","NDCLHackingExercisesTwo","NDCLHackingExercises-Three"]
+namePrefix = [
+  "NDCLHackingExercises",
+  "NDCLHackingExercisesTwo",
+  "NDCLHackingExercises-Three"
+]
