2d0deea1d740a3598d95361120c8b6093a6300da
Thomas Kennedy <thomaskennedy1066@gmail.com>
2022-03-15 20:13:44 -0400

    removed 3rd web app.  only having two for this

--- prod.tf
+++ prod.tf
@@ -78,21 +78,3 @@ resource "azurerm_app_service" "HackingExercisesTwo" {
     "Terraform" : "true"
   }
 }
-
-resource "azurerm_app_service" "HackingExercisesThree" {
-  name                = "webapp-${var.namePrefix[2]}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-
-  source_control {
-    repo_url           = "https://github.com/thomaskennedy1066/nodejs-docs-hello-world.git"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
-  }
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
